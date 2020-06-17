1681b7461680d0f83958734bab1ed0798e682fea
progrhyme <progrhyme@gmail.com>
2020-06-18 03:38:43 +0900

    shelp 0.5.2

--- Formula/shelp.rb
+++ Formula/shelp.rb
@@ -1,13 +1,13 @@
 class Shelp < Formula
-  Version = '0.5.1'
+  Version = '0.5.2'
   version Version
 
   if OS.mac?
     Binary = "shelp_#{Version}_darwin_x86_64"
-    sha256 "70207befa0f7ad8f5366a5caa5ac4c76f11420ab3a2e01b5a1cd83281904b0b2"
+    sha256 "3ebe49bd00218fe86a9a2c72063af3b52a5cf53532bbf0016b342626ebab9fc2"
   elsif OS.linux?
     Binary = "shelp_#{Version}_linux_x86_64"
-    sha256 "39f1bc19e6bad4c22a37b67565f40c8b58a1720e845c988a5b31a53973a8ef3a"
+    sha256 "ab674789e705dc67eb3184957b866c47f9cf59f1f10f83092cf0407998f274ec"
   end
 
   url "https://github.com/progrhyme/shelp/releases/download/v#{Version}/#{Binary}"
