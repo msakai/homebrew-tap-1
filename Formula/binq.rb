cf6a441b970d53e1d91e4bd1544f899bee90e949
progrhyme <progrhyme@gmail.com>
2020-07-06 22:46:44 +0900

    binq 0.6.0

--- Formula/binq.rb
+++ Formula/binq.rb
@@ -1,13 +1,13 @@
 class Binq < Formula
-  Version = '0.5.0'
+  Version = '0.6.0'
   version Version
 
   if OS.mac?
     Archive = "binq_#{Version}_darwin_amd64.zip"
-    sha256 "fe5528f8f5ac58bd1698b5ff91ea71269fbe077bc8d9b48cc588a644d26446bd"
+    sha256 "c6250278a25714d8022c076442bc8274a3389677f8a5afbb70c197750db020a2"
   elsif OS.linux?
     Archive = "binq_#{Version}_linux_amd64.zip"
-    sha256 "3a4379e2dcf5b51f063ce6ac44eb4855b7994929ef6cf1a9763507fc6cc6cb7a"
+    sha256 "b6315a2ab0748e732aa9ed7c754c65fea92f791195a09eea600437a3e4e2d1d0"
   end
 
   url "https://github.com/progrhyme/binq/releases/download/v#{Version}/#{Archive}"
