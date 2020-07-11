bc16e85b6c418fc6c9782404218b264e52fb459c
progrhyme <progrhyme@gmail.com>
2020-07-11 20:51:27 +0900

    binq 0.6.1

--- Formula/binq.rb
+++ Formula/binq.rb
@@ -1,17 +1,17 @@
 class Binq < Formula
-  Version = '0.6.0'
+  Version = '0.6.1'
   version Version
 
   if OS.mac?
     Archive = "binq_#{Version}_darwin_amd64.zip"
-    sha256 "c6250278a25714d8022c076442bc8274a3389677f8a5afbb70c197750db020a2"
+    sha256 "ec67b94392a545deb80ea45f935c72fa3e6711eb55bd1459e6008789ae4bde14"
   elsif OS.linux?
     Archive = "binq_#{Version}_linux_amd64.zip"
-    sha256 "b6315a2ab0748e732aa9ed7c754c65fea92f791195a09eea600437a3e4e2d1d0"
+    sha256 "789b8dc86cf49f5f5d0eb6f8c5766c126664378ff5fbfe7c328b37af9956186b"
   end
 
   url "https://github.com/progrhyme/binq/releases/download/v#{Version}/#{Archive}"
-  desc 'Git-based package manager for shell scripts written in Go'
+  desc 'Download and Extract Binary or Archive Items'
   homepage 'https://github.com/progrhyme/binq'
   head 'https://github.com/progrhyme/binq.git'
 
